ac55402bb5b5f04b77bcd1f52f8aa60f5d9a9902
seunghunbang <upi21x@gmail.com>
2021-06-21 13:03:45 +0300

    parcel practice

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// ESM -- import/export = 일반 js
+// CommonJS --require()/module.exports = 노드 js
+
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
